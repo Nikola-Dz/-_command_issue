96a586e118f1c0b2b1585f3596c81cf23ce35236
Nikola-Dz <nkl.dzamic@gmail.com>
2024-03-25 13:14:55 +0100

    test: update test.e2e.js

--- test/test.e2e.js
+++ test/test.e2e.js
@@ -8,29 +8,29 @@ describe(`$$ command issue`, function() {
     
     context(`Paragraphs exist in DOM`, async function() {
         
-        it(`Case 1 (Synchronous assignment)`, async function() {
+        it(`Case 1 - toBeDisplayed (Synchronous assignment)`, async function() {
             
             paragraphs1 = $$('.div-holder-1 .paragraph-class');
             await expect(paragraphs1[0]).toBeDisplayed();
         })
         
-        it(`Case 2 (Asynchronous assignment)`, async function() {
+        it(`Case 2 - toBeDisplayed (Asynchronous assignment)`, async function() {
             
             paragraphs1 = await $$('.div-holder-1 .paragraph-class');
             await expect(paragraphs1[0]).toBeDisplayed();
         })
     })
     
-    context(`Paragraphs exist in DOM`, async function() {
+    context(`Paragraphs don't exist in DOM`, async function() {
         
-        it(`Case 1 (Synchronous assignment)`, async function() {
+        it(`Case 1 - not.toBeDisplayed (Synchronous assignment)`, async function() {
             
             // This doesn't work in v8.35.0
             paragraphs2 = $$('.div-holder-2 .paragraph-class');
             await expect(paragraphs2[0]).not.toBeDisplayed();
         })
         
-        it(`Case 2 (Asynchronous assignment)`, async function() {
+        it(`Case 2 - not.toBeDisplayed (Asynchronous assignment)`, async function() {
             
             paragraphs2 = await $$('.div-holder-2 .paragraph-class');
             await expect(paragraphs2[0]).not.toBeDisplayed();
